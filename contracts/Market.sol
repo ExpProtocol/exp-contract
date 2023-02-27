8bbb5ddc35f3e9d803cf09392fbabb090e632538
inaridiy <inainainariinainari@gmail.com>
2023-02-26 18:32:39 -0700

    feat

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IIMarket.sol";
 import "./interfaces/IAdapter.sol";
 
-contract Market is IMarket, Context {
+contract Market is IMarket, Context, EIP712 {
   uint96 private _totalLend;
   mapping(uint96 => Lend) public lends;
   mapping(uint96 => RentContract) public rentContracts;
-  mapping(address => mapping(uint24 => bool)) public usedNonces;
+  mapping(address => uint24) public usedNonces;
 
   bytes32 constant GUARANT_REQUEST_TYPE_HASH =
     keccak256(
@@ -24,6 +25,8 @@ contract Market is IMarket, Context {
         ")"
       )
     );
+
+  constructor() EIP712("EXP-Market", "1") {}
 
   function _blockTimeStamp() private returns (uint96) {
     return uint96(block.timestamp);
@@ -63,6 +66,42 @@ contract Market is IMarket, Context {
       lend.adapter.isBorrowable(_msgSender(), lend.token, lend.data),
       "Not borrowable"
     );
+
+    bytes32 guarantDigest = _hashTypedDataV4(
+      keccak256(
+        abi.encode(
+          GUARANT_REQUEST_TYPE_HASH,
+          lendId,
+          guarantBalance,
+          guarantFee,
+          usedNonces[guarantor] + 1
+        )
+      )
+    );
+
+    require(
+      ECDSA.recover(guarantDigest, signature) == guarantor,
+      "Invalid signature"
+    );
+
+    lend.payment.transferFrom(
+      _msgSender(),
+      lend.lender,
+      lend.totalPrice - guarantBalance
+    );
+    lend.payment.transferFrom(guarantor, lend.lender, guarantBalance);
+
+    lend.adapter.lendTransfer(lend.lender, lend.token, _msgSender(), lend.data);
+    rentContracts[lendId] = RentContract({
+      renter: _msgSender(),
+      startTime: _blockTimeStamp(),
+      guarantor: address(0),
+      balance: lend.totalPrice,
+      guarantBalance: 0,
+      guarantFee: 0
+    });
+
+    usedNonces[guarantor]++;
   }
 
   function registerToLend(
