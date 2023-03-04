c15a84095145b7482629ee892275b179ab47d567
inaridiy <inainainariinainari@gmail.com>
2023-03-04 09:59:33 -0700

    update deploy scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const Receiver = await ethers.getContractFactory("Only1155Receiver");
+  const receiver = await Receiver.deploy();
+  await receiver.deployed();
+
   const ERC721Adapter = await ethers.getContractFactory("ERC721Adapter");
   const erc721Adapter = await ERC721Adapter.deploy();
   await erc721Adapter.deployed();
@@ -10,15 +14,16 @@ async function main() {
   await erc1155Adapter.deployed();
 
   const Market = await ethers.getContractFactory("Market");
-  const market = await Market.deploy(
-    erc721Adapter.address,
-    erc1155Adapter.address
-  );
+  const market = await Market.deploy(receiver.address, erc721Adapter.address, erc1155Adapter.address);
   await market.deployed();
+
+  await market.setSupportedReceiveSelector(receiver.interface.getSighash("onERC1155Received"), true);
+  await market.setSupportedReceiveSelector(receiver.interface.getSighash("onERC1155BatchReceived"), true);
 
   console.log("Market deployed to:", market.address);
   console.log("ERC721Adapter deployed to:", erc721Adapter.address);
   console.log("ERC1155Adapter deployed to:", erc1155Adapter.address);
+  console.log("Receiver deployed to:", receiver.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
